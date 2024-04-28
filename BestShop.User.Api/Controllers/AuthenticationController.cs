d32ef0804946beb2f253fad57655c7ae4ac86c5c
Administrator <Administrator@PC-12>
2024-04-28 19:58:18 +0330

    Add User Managment Service

---
+++ BestShop.User.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,40 @@
+﻿
+using BestShop.User.Application.DTOs;
+using BestShop.User.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace BestShop.User.Api.Controllers;
+
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    [HttpPost]
+    [Route("")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> Login([FromBody] LoginDto dto)
+    {
+        _authenticationService.Login(dto);
+        return Ok();
+    }
+
+
+    [HttpPost]
+    [Route("")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> Register([FromBody] RegisterDto dto)
+    {
+        _authenticationService.Register(dto);
+        return Ok();
+    }
+}
---
+++ BestShop.User.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,23 @@
+﻿using BestShop.User.Application.Interfaces;
+using BestShop.User.Domain.Settings;
+using BestShop.User.Infrastructure.Context;
+using BestShop.User.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BestShop.User.Infrastructure;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection RegisterInfrastructureServices(this IServiceCollection services,IConfiguration configuration, string connectionString)
+    {
+        services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(connectionString));
+
+        services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
+
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        return services;
+    }
+}
